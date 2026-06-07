c54074ade7a598bfc2f7faa00aabc5523a7acfa8
RedstoneMaster011 <redstonemaster109@gmail.com>
2026-06-07 00:28:57 -0400

    added support for 1.21.11-1.20.1 with mutiloader support! 1.20.6-1.20.1 needs testing

--- src/main/java/dev/redstone/hairphysics/Hairphysics.java
+++ src/main/java/dev/redstone/hairphysics/Hairphysics.java
@@ -1,16 +1,52 @@
 package dev.redstone.hairphysics;
 
+import dev.redstone.hairphysics.client.HairphysicsClient;
+//? if fabric_like
 import net.fabricmc.api.ModInitializer;
+//? if neoforge {
+/*import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+*///?}
+//? if forge
+/*import net.minecraftforge.fml.common.Mod;
+*/
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+//? if forge_like
+/*@Mod(Hairphysics.FORGE_MOD_ID)
+*///? if fabric_like
 public class Hairphysics implements ModInitializer {
+//? if forge_like
+/*public class Hairphysics {
+*/
     public static String MOD_ID = "hair-physics";
+    public static final String FORGE_MOD_ID = "hair_physics";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+//? if fabric_like {
     @Override
     public void onInitialize() {
+        init();
+    }
+//?}
+
+//? if neoforge {
+    /*public Hairphysics(IEventBus modBus) {
+        init();
+        HairphysicsClient.registerNeoForge(modBus);
+    }
+    *///?}
+
+//? if forge {
+    /*public Hairphysics() {
+        init();
+        HairphysicsClient.registerForge();
+    }
+    *///?}
+
+    private static void init() {
         LOGGER.info("bros hair is starting up");
     }
 }
